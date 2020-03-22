f800e7f9abe648a44e1fd576697954e7739af512
luke <luke21608@gmail.com>
2020-03-22 15:58:53 +0800

    update

--- dbtest.js
+++
@@ -1,9 +0,0 @@
-console.log("***lukegit***");
-console.log("*************");
-
-var mongoose = require('mongoose');
-var mongoDB = 'mongodb://localhost:27017/mongo';
-mongoose.connect(mongoDB);
-mongoose.Promise = global.Promise;
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
